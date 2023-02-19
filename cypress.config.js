9a0f93b98806f2e016aaf2e9dc2a924b8e3a8e76
LAPTOP-80ET940E\PC <hoangmy1209@gmail.com>
2023-02-19 11:27:41 +0700

    add report - is testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,33 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'reporters/custom.js',
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    charts: true,
+    embeddedScreeshots: true,
+    inlineAssets: true
+  },
+
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  reporterOptions: {
+    charts: true,
+    embeddedScreeshots: true,
+    inlineAssets: true
+  },
+
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output-[hash].xml' 
+    //--reporter-options "mochaFile=results/my-test-output-[hash].xml"
+  },
+
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com',
     setupNodeEvents(on, config) {
+      // require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,5 +17,8 @@
 import './commands'
 
 import 'cypress-real-events'
+
+import 'cypress-mochawesome-reporter/register'
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
