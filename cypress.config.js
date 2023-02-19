a41e43dfbe6efab0c7a2e1df3bec5aadf3d8827b
LAPTOP-80ET940E\PC <hoangmy1209@gmail.com>
2023-02-19 12:45:49 +0700

    revise report in to cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,35 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'reporters/custom.js',
 
-  reporter: 'mochawesome',
-  reporterOptions: {
-    charts: true,
-    embeddedScreeshots: true,
-    inlineAssets: true
-  },
+  // reporter: 'mochawesome',
+  // reporterOptions: {
+  //   charts: true,
+  //   embeddedScreeshots: true,
+  //   inlineAssets: true
+  // },
 
   reporter: 'cypress-mochawesome-reporter',
   video: true,
   reporterOptions: {
     charts: true,
-    embeddedScreeshots: true,
+    embeddedScreenshots: true,
     inlineAssets: true
   },
 
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'results/my-test-output-[hash].xml' 
+  // reporter: 'junit',
+  // reporterOptions: {
+  //   mochaFile: 'results/my-test-output-[hash].xml' 
     //--reporter-options "mochaFile=results/my-test-output-[hash].xml"
-  },
+  // },
 
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com',
     setupNodeEvents(on, config) {
-      // require('cypress-mochawesome-reporter/plugin')(on)
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
   defaultCommandTimeout: 10000,
-  pageLoadTimeout: 20000
+  pageLoadTimeout: 20000,
 });
