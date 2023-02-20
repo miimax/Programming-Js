1d9663bd55fd56a496f2f305607b7987811c0766
LAPTOP-80ET940E\PC <hoangmy1209@gmail.com>
2023-02-20 09:30:44 +0700

    add report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    inlineAssets: true,
+    embeddedScreeshots: true,
+  },
+
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin') (on)
       // implement node event listeners here
     },
   },
